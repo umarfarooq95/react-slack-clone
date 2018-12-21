788c81eaa2e304189460a23b8aff75825ac4281d
umarfarooq95 <mohamedumar931@gmail.com>
2018-12-21 10:13:38 +0530

    implemented userpanel component and side panel component is process

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import {Menu} from 'semantic-ui-react'
+import UserPanel from './UserPanel';
 class SidePanel extends React.Component{
     render (){
         return (
-            <div> Side Panel </div>
+          <Menu size="large"
+          inverted
+          fixed='left'
+          vertical
+          style={{background : '#4c3c4c',fontSize: '1.rem'}}>
+            <UserPanel/>
+          </Menu>
         )
     }
 }
---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+
+
+class UserPanel extends React.Component{
+    dropdownOptions = () => [
+        {
+            key :'user',
+            text : <span>Signed in as <strong>User</strong></span>,
+            disabled : true
+        },
+        {
+            key :'changeavatar',
+            text : <span>Change Avatar</span>
+        },
+        {
+            key :'signout',
+            text : <span>Sign Out</span>
+        }
+    ]
+    render (){
+        return (
+            <Grid style={{background : '#4c3c4c'}}>
+                <Grid.Column>
+                    <Grid.Row style={{padding : '1.2em', margin:0}}>
+                        <Header inverted floated='left' as='h2'>
+                            <Icon name='code'/>
+                            <Header.Content>DevChat</Header.Content>
+                        </Header>
+                    </Grid.Row>
+                    <Header inverted style={{padding: '0.25em'}} as='h4'>
+                        <Dropdown
+                        trigger={<span>User</span>}
+                        options={this.dropdownOptions()}
+                        />
+                </Header>
+                </Grid.Column>
+            </Grid>
+        )
+    }
+}
+
+export default UserPanel
